b424d46a6e675a139d1629ebd8e6ef6038269016
salafey <118829558+salafey@users.noreply.github.com>
2023-01-07 13:26:07 +0100

    3 files modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/style.scss';
 import NavBar from '../components/navigation';
 import Footer from '../components/footer';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
       <NavBar />
       <Component {...pageProps} />
       <Footer />
+      <Analytics />
     </>
   );
 }
