ee6e73c3082344e6a8bdd0104e361cc5641f5ad3
salafey <118829558+salafey@users.noreply.github.com>
2023-01-12 16:36:48 +0100

    3 files modified, 1 added, 3 deleted

--- pages/evenements/readarts/index.jsx
+++ pages/evenements/readarts/index.jsx
@@ -3,7 +3,40 @@ import Paragraph from '../../../components/paragraph';
 import Titre from '../../../components/titre';
 import Banniere from '../composants/banniere';
 
-import gallerie from './gallerie.json';
+const gallerie = [
+  {
+    "id": 1,
+    "image": "https://media.graphassets.com/oj4Tn0CQS2WEtuMMfr2r"
+  },
+  {
+    "id": 2,
+    "image": "https://media.graphassets.com/r23WuNONS1mf5aznEl6O"
+  },
+  {
+    "id": 3,
+    "image": "https://media.graphassets.com/KmIbEFqXSPawYvmpjU0h"
+  },
+  {
+    "id": 4,
+    "image": "https://media.graphassets.com/Gse0mfwmTQGEKVgX6uJg"
+  },
+  {
+    "id": 5,
+    "image": "https://media.graphassets.com/A06T7YPVSnKdyCshW22q"
+  },
+  {
+    "id": 6,
+    "image": "https://media.graphassets.com/ZhPyB2TkRcO8V9pWCVBn"
+  },
+  {
+    "id": 7,
+    "image": "https://media.graphassets.com/e4EV8Se8Q6eQ9SzlpRWz"
+  },
+  {
+    "id": 8,
+    "image": "https://media.graphassets.com/SxcVFNwDQSmGLDzDt5xM"
+  }
+]
 
 export default function EventArticle() {
   const Element = gallerie.map((el) => (
--- pages/evenements/salafey_2022/index.jsx
+++ pages/evenements/salafey_2022/index.jsx
@@ -11,7 +11,40 @@ import Titre, {
 } from '../../../components/titre';
 import Telecharger from '../composants/telecharger';
 
-import gallerie from './gallerie.json';
+const gallerie = [
+  {
+    "id": 1,
+    "image": "https://media.graphassets.com/oj4Tn0CQS2WEtuMMfr2r"
+  },
+  {
+    "id": 2,
+    "image": "https://media.graphassets.com/r23WuNONS1mf5aznEl6O"
+  },
+  {
+    "id": 3,
+    "image": "https://media.graphassets.com/KmIbEFqXSPawYvmpjU0h"
+  },
+  {
+    "id": 4,
+    "image": "https://media.graphassets.com/Gse0mfwmTQGEKVgX6uJg"
+  },
+  {
+    "id": 5,
+    "image": "https://media.graphassets.com/A06T7YPVSnKdyCshW22q"
+  },
+  {
+    "id": 6,
+    "image": "https://media.graphassets.com/ZhPyB2TkRcO8V9pWCVBn"
+  },
+  {
+    "id": 7,
+    "image": "https://media.graphassets.com/e4EV8Se8Q6eQ9SzlpRWz"
+  },
+  {
+    "id": 8,
+    "image": "https://media.graphassets.com/SxcVFNwDQSmGLDzDt5xM"
+  }
+]
 
 export default function Apropos() {
   const Element = gallerie.map((el) => (
